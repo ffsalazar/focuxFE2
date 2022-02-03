76c85c05bbe683978f6dad6bdfa2a35fda3f5ac9
Jesús Pelay <jpelay@intelix.biz>
2022-02-03 11:33:14 -0400

    añadiendo tipo cliente y tipo de negocio

--- src/app/modules/admin/dashboards/collaborators/collaborators.types.ts
+++ src/app/modules/admin/dashboards/collaborators/collaborators.types.ts
@@ -68,3 +68,21 @@ export interface Phone
     type: string;
     isActive: number;
 }
+
+export interface Client
+{
+    id: number;
+    name: string;
+    description: string;
+    isActive: number;
+    businessType: BusinessType;
+}
+
+export interface BusinessType
+{
+    id: number;
+    code: string;
+    name: string;
+    description: string;
+    isActive: number;
+}
